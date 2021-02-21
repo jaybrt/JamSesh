07e1b90db48441842378ac6d40d023bf3246f192
Jay Landers <jay@jaybirdgroup.net>
2021-02-21 02:30:49 -0700

    started backed with djagno and added room model

---
+++ jam_sesh/jam_sesh/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jam_sesh project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jam_sesh.settings')
+
+application = get_asgi_application()
