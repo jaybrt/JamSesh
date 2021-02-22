907f6b0179bba0ea1b801524d5a3dafa89418228
Jay Landers <jay@jaybirdgroup.net>
2021-02-21 18:44:12 -0700

    Started react frontend

--- jam_sesh/api/serializers.py
+++ jam_sesh/api/serializers.py
@@ -6,3 +6,4 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+ 
